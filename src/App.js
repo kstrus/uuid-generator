7f18bccb792d4e786da9002297a619612cb46546
Strus <katarzyna.strus@sgdigital.com>
2020-10-23 22:31:22 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+const wrapperStyles = {
+    fontFamily: 'sans-serif',
+    backgroundColor: '#000',
+    maxWidth: 600,
+    textAlign: 'center',
+    margin: '20px auto',
+    padding: 30,
+    color: '#fff'
+};
+
+const infoStyles = {
+    color: '#999'
+};
+
+const uuidStyles = {
+    fontSize: 30
+};
+
+const buttonStyles = {
+    padding: '10px 15px',
+    cursor: 'pointer'
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [ uuid, setUuid ] = useState(uuidv4());
+
+    return (
+        <div style={wrapperStyles}>
+            <p style={infoStyles}>Your version 4 UUID:</p>
+            <p style={uuidStyles}>{uuid}</p>
+            <button style={buttonStyles} onClick={() => setUuid(uuidv4())}>Generate</button>
+        </div>
+    );
 }
 
 export default App;
